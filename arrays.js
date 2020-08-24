5fbc47b3c559ba18db8fefb95e7e222d1944d91e
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-24 20:38:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { function chocolateBars.shift(); 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { function chocolateBars.shift();
   return chocolateBars; 
 }
