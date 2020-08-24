a1f2c688014fb71a627411c9dc0ded251830e385
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-24 20:47:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,5 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { chocola
 
 function removeElementFromBeginningOfArray(chocolateBars) { return chocolateBars.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) { return chocolateBars.pop(); }
