b9577da498012373d0e76bb3f294cef474aa5c85
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-24 20:39:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { function chocolateBars.shift();
-  return chocolateBars; 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { chocolateBars.shift(); 
+  return chocolateBars;
 }
