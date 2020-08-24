2cd1015214839fb7164b6355f9489486ef50753b
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-24 20:19:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,4 +15,4 @@ function addElementToEndOfArray(chocolateBars, mars) { return [...chocolateBars,
 
 function destructivelyAddElementToEndOfArray(chocolateBars, mars) { chocolateBars.push(mars); return chocolateBars; }
 
-function accessElementInArray(chocolateBars, [2]) { return (chocolateBars[2] }
+function accessElementInArray(array, index) { return (array[index] }
