9494d0b932daa521b19bfcb9f0b2756b8847dcb2
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-24 20:07:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,5 @@ var chocolateBars = [
 var mars = "mars"
 
 function addElementToBeginningOfArray(chocolateBars, mars) { return [mars, ...chocolateBars] }
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, mars) { return chocolateBars(mars,...chocolateBars)}
